bf5d96fb8b9f24870413fad2e7d6e1c7530c4e18
sigint9 <dev@null.com>
2016-06-06 00:59:10 +0100

    Fix 100% CPU usage when idle.

--- src/asio_events.cpp
+++ src/asio_events.cpp
@@ -6,13 +6,20 @@ AsioEvents::AsioEvents(boost::asio::io_service& io) : m_io(io)
 {
   m_timer = boost::make_shared<QTimer>();
   connect(m_timer.get(), SIGNAL(timeout()), this, SLOT(timeout()));
-  m_timer->setInterval(0);
-  m_timer->start();
+  m_timer->setSingleShot(true);
+  timeout();
 }
 
 void AsioEvents::timeout()
 {
   boost::system::error_code error;
-  m_io.poll_one(error);
+  size_t count = m_io.poll_one(error);
   m_io.reset();
+
+  /* if we are busy, poll quickly */
+  if (count) {
+    m_timer->start(0);
+  } else {
+    m_timer->start(1000/25);
+  }
 }
